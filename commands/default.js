5be9ec4e670193037be78c01f8dff5235d975f7a
lanmower <almagestfraternite@gmail.com>
2019-06-09 10:23:50 +0200

    done

--- commands/default.js
+++ commands/default.js
@@ -18,7 +18,7 @@ module.exports = {
             let message = log.res.logs.message;
             if(message == '') message = null;
             if (log && log.res.logs.message) {
-                msg.reply(`message \n\`https://bloks.io/transaction/05a95727de597af51026bfffd950afce2d3a53de53afea15270e5961afa02dc8\``).catch(e => { console.error(e) });
+                msg.reply(`${message} \nhttps://bloks.io/transaction/05a95727de597af51026bfffd950afce2d3a53de53afea15270e5961afa02dc8`).catch(e => { console.error(e) });
             }
         } catch(e) {
             console.error(e);
