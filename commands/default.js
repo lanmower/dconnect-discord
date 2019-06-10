67f5e381a58e1f2d916f456511d609ef095def91
lanmower <almagestfraternite@gmail.com>
2019-06-10 17:11:47 +0200

    fixes to logs

--- commands/default.js
+++ commands/default.js
@@ -14,7 +14,7 @@ module.exports = {
         try {
             console.log("RUNNING", app, key);
             const log = await runContract(app, key, { author, channel, server, data: words }, dbo);
-            console.log("LOG", log);
+            //console.log("LOG", log);
             let message = log.res.logs.message;
             if(message == '') message = null;
             if (log && log.res.logs.message) {
