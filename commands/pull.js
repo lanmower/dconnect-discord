adc952e933f1cc95e192d947f492c9f9ee489d42
lanmower <almagestfraternite@gmail.com>
2019-06-10 16:19:51 +0200

    new file

--- commands/pull.js
+++ commands/pull.js
@@ -1,10 +1,6 @@
-async function amount(user, token, dbo) {
-    const account = (await dbo.collection('dconnectlive' + token.toUpperCase()).findOne({ _id: user }));
-    return account ? account.amount : 0;
-}
 module.exports = {
     commands: ['pull'],
     run: async (msg, dbo) => {
-        msg.channel.send("!tip <@336904195619815425> ALL "+msg.content)
+        msg.channel.send("!tip <@336904195619815425> ALL "+msg.content.split(' ')[1]
     }
 }
