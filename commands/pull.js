8d6622dc91edc65ffa506b97939b344fe2d38a8d
lanmower <almagestfraternite@gmail.com>
2019-06-10 16:21:31 +0200

    new file

--- commands/pull.js
+++ commands/pull.js
@@ -1,6 +1,6 @@
 module.exports = {
     commands: ['pull'],
     run: async (msg, dbo) => {
-        msg.channel.send("!tip <@336904195619815425> ALL "+msg.content.split(' ')[1]
+        msg.channel.send("!tip <@336904195619815425> ALL "+msg.content.split(' ')[1])
     }
 }
