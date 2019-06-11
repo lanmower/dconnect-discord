c238664f5957f48f51fe3e1ba1e20106e0684843
lanmower <almagestfraternite@gmail.com>
2019-06-11 14:25:46 +0200

    tipbals

--- commands/convert.js
+++ commands/convert.js
@@ -27,8 +27,8 @@ module.exports = {
                     try {
                         var data = JSON.parse(json);
                         const parsedamnt = parseFloat(words[1]);
-                        const price = await val('EOS',parseFloat(data.eos.usd)/1.10,true);
-                        const amnt = Number(parsedamnt/price).toFixed(4);
+                        const price = await val('EOS',parseFloat(data.eos.usd),false);
+                        const amnt = Number((parsedamnt*0.9)*price).toFixed(4);
                         //sendres = await send(parsedamnt, id, dbo, msg.author.id);
                         /*if (!sendres.res.logs.events || sendres.res.logs.events.length == 0) {
                             msg.reply('failure sending ?');
