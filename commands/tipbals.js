3e023b7515e288d8da8630b645acc471cb28f62b
lanmower <almagestfraternite@gmail.com>
2019-06-12 14:40:12 +0200

    done

--- commands/tipbals.js
+++ commands/tipbals.js
@@ -5,6 +5,10 @@ module.exports = {
         waiting.push({time:new Date().getTime(), expiry:180000, run:(msg)=>{
             const words = msg.content.split(' ');
             console.log(words);
+            if(msg.author.id == '512212602613399552') {
+                return false;
+            }
+            return true;
         }})
     }
 }
