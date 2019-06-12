cf49abadfffdbd008f592694cdd613a80bd849ae
lanmower <almagestfraternite@gmail.com>
2019-06-12 14:41:53 +0200

    done

--- commands/tipbals.js
+++ commands/tipbals.js
@@ -3,9 +3,8 @@ module.exports = {
     run: async (msg, dbo) => {
         msg.channel.send("!bals");
         waiting.push({time:new Date().getTime(), expiry:180000, run:(msg)=>{
-            const words = msg.content.split(' ');
-            console.log(words);
             if(msg.author.id == '512212602613399552') {
+                console.log("FOUND", msg.content);
                 return false;
             }
             return true;
